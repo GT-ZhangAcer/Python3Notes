341952e870c7baff079666a7a77652a6578aef3b
GT-ZhangAcer <1029550448@qq.com>
2019-01-19 17:44:45 +0800

    UPData

--- Class_OpenCV/o4_ROI建立_尺寸获取.py
+++ Class_OpenCV/o4_ROI建立_尺寸获取.py
@@ -9,9 +9,13 @@ img=cv.imread(file)
 size=img.shape#获取图像形状信息 返回长宽和通道数
 print("图像尺寸为："+str(size))
 
-#除法必须得//
-imgroi=img[:size[0],:size[1]//2]#高度选取上半段
-
+#切割图像
+#除法必须得a//b
+imgroi=img[:size[0],:size[1]//2]#宽度选取上半段
+imgroi2=img[:size[0],:size[1]//2]
 cv.imshow("1",imgroi)
 
+#拼合图像
+addimg=cv.addWeighted(imgroi,0.3,imgroi2,0.1,0)#第一个参数为底图 第二个参数为底图透明度 第三个和第四个同理为贴图 第五个为Gamma，权重计算后的附加值
+cv.imshow("2",addimg)
 cv.waitKey(0)#等待按键后关闭窗口
