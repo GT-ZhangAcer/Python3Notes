70c69f39d7626355b9ec652249f80cafc3f0bc88
GT_Console <1029550448@qq.com>
2019-03-14 15:23:39 +0800

    updata

--- Class_PaddlePaddle/test02_img/predit.py
+++ Class_PaddlePaddle/test02_img/predit.py
@@ -1,46 +1,37 @@
-#加载库
+# 加载库
 import paddle.fluid as fluid
 import numpy as np
 import Class_OS.o1_获得当前工作目录
 from PIL import Image
-
-#指定路径
-path=Class_OS.o1_获得当前工作目录.main()
-params_dirname = path+"test02.inference.model"
-print("训练后文件夹路径"+params_dirname)
-#目标数据
+import numpy
+# 指定路径
+path = Class_OS.o1_获得当前工作目录.main()
+params_dirname = path + "test02.inference.model"
+print("训练后文件夹路径" + params_dirname)
+# 目标数据
 with open(path + "data/ocrData.txt", 'rt') as f:
-    a=f.read()
+    a = f.read()
 
 
-def load_image(i):
-    file=path + "data/%s.jpg"%i
-    img = Image.open(file)
-    # 统一图像大小
-    img = img.resize((15, 30), Image.ANTIALIAS)
-    # 转换成numpy值
-    img = np.array(img).astype(np.float32)
-    # 转换成CHW
-    img = img.transpose((2, 0, 1))
-    # 转换成BGR
-    img = img[(2, 1, 0), :, :] / 255.0
-    img = np.expand_dims(img, axis=0)
-    return img
+def dataReader(i):
+    im = Image.open(path + "data/" + str(i + 1) + ".jpg").convert('L')
+    im = np.array(im).reshape(30, 15).astype(np.float32)
+    im = im / 255.0 * 2.0 - 1.0
+    im = numpy.expand_dims(im, axis=0)
+    return im
 
 
-#参数初始化
+# 参数初始化
 cpu = fluid.CPUPlace()
 exe = fluid.Executor(cpu)
-prog=fluid.default_startup_program()
+prog = fluid.default_startup_program()
+exe.run(prog)
 
 # 加载模型
-[inference_program, feed_target_names,fetch_targets] = fluid.io.load_inference_model(params_dirname, exe)
+[inference_program, feed_target_names, fetch_targets] = fluid.io.load_inference_model(params_dirname, exe)
 
-
-
-for i in range(1999,2000):
-    img=load_image(i)
-    results = exe.run(inference_program,
-                      feed={feed_target_names[0]: img},
-                      fetch_list=fetch_targets)
-    print(results[1])
+img=dataReader(1998)
+results = exe.run(inference_program,
+    feed={feed_target_names[0]: img},
+    fetch_list=fetch_targets)
+print(results)
--- Class_PaddlePaddle/test02_img/trainE0.py
+++ Class_PaddlePaddle/test02_img/trainE0.py
@@ -1,102 +1,118 @@
-# 加载库
 import paddle.fluid as fluid
+import paddle
 import numpy as np
-import Class_OS.o1_获得当前工作目录
 from PIL import Image
+import os
 
-# 指定路径
-path = Class_OS.o1_获得当前工作目录.main()
-params_dirname = path + "test02.inference.model"
+#定义网络
+def vgg_bn_drop(input):
+    def conv_block(ipt, num_filter, groups, dropouts):
+        return fluid.nets.img_conv_group(
+            input=ipt,
+            pool_size=2,
+            pool_stride=2,
+            conv_num_filter=[num_filter] * groups,
+            conv_filter_size=3,
+            conv_act='relu',
+            conv_with_batchnorm=True,
+            conv_batchnorm_drop_rate=dropouts,
+            pool_type='max')
+
+    conv1 = conv_block(input, 64, 2, [0.3, 0])
+    conv2 = conv_block(conv1, 128, 2, [0.4, 0])
+    conv3 = conv_block(conv2, 256, 3, [0.4, 0.4, 0])
+    conv4 = conv_block(conv3, 512, 3, [0.4, 0.4, 0])
+    conv5 = conv_block(conv4, 512, 3, [0.4, 0.4, 0])
+
+    drop = fluid.layers.dropout(x=conv5, dropout_prob=0.5)
+    fc1 = fluid.layers.fc(input=drop, size=512, act=None)
+    bn = fluid.layers.batch_norm(input=fc1, act='relu')
+    drop2 = fluid.layers.dropout(x=bn, dropout_prob=0.5)
+    fc2 = fluid.layers.fc(input=drop2, size=512, act=None)
+    predict = fluid.layers.fc(input=fc2, size=10, act='softmax')
+    return predict
+
+params_dirname ="./test02.inference.model"
 print("训练后文件夹路径" + params_dirname)
 # 参数初始化
 cpu = fluid.CPUPlace()
 exe = fluid.Executor(cpu)
 
-# 数据处理
-'''
-imglst = []  # 图片列表
-textlst = []  # 文字列表
-# textlst = np.loadtxt(path + "data/ocrData.txt", dtype=int)  # 文字列表 因为含有0开头的数字 所以不能是数值类型
+datatype='float32'
+#加载数据
 
-for i in range(3):
-    image = Image.open(path + "datawb/" + str(i) + ".jpg")
-    image = np.array(image).reshape(1,1,70, 30).astype(np.float32)
-    imglst.append(image)
-imgdata=np.array([])
-imgdata = np.append(imglst, dtype=float)
+with open("./data/ocrData.txt", 'rt') as f:
+    a=f.read()
+def dataReader():
+    def redaer():
+        for i in range(1500):
+            im = Image.open("./data/" + str(i+1) + ".jpg").convert('L')
+            im = np.array(im).reshape(30, 15).astype(np.float32)
+            im = im / 255.0 * 2.0 - 1.0
+            '''
+            img = paddle.dataset.image.load_image(path + "data/" + str(i+1) + ".jpg")
+            img=np.transpose(img, (2, 0, 1))'''
+            labelInfo=a[i]
+            yield im,labelInfo
+    return redaer
 
-with open(path + "data/ocrData.txt") as f:
-    for line in f:
-        temp=[]
-        temp.append(line[0])
-        temp=np.array(temp,dtype=int)
-        textlst.append(temp)#找了一晚上为什么出现 could not convert string to float: 'B' 发现原识别中出现了非数字的字符
-textlst=np.array(textlst,dtype=int)
-textlst.astype('int64')
-'''
-imglst=np.array([[[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]]).astype('float32')
-textlst = np.array([[0], [1], [2]]).astype('int64')
-print(imglst)
-print(textlst)
-# 定义输入层
-image = fluid.layers.data(name='image', shape=[1, 5, 2], dtype='float32')  # 单通道 70x30图片
+def testReader():
+    def redaer():
+        for i in range(1501,1800):
+            im = Image.open("./data/" + str(i+1) + ".jpg").convert('L')
+            im = np.array(im).reshape(30, 15).astype(np.float32)
+            im = im / 255.0 * 2.0 - 1.0
+            '''
+            img = paddle.dataset.image.load_image(path + "data/" + str(i+1) + ".jpg")
+            img=np.transpose(img, (2, 0, 1))'''
+            labelInfo=a[i]
+            yield im,labelInfo
+    return redaer
+#定义网络
+x = fluid.layers.data(name="x",shape=[1,30,15],dtype=datatype)
 label = fluid.layers.data(name='label', shape=[1], dtype='int64')
 
-'''
-# 卷积神经网络
-def convolutional_neural_network(input):
-    # 第一个卷积层，卷积核大小为3*3，一共有32个卷积核
-    conv1 = fluid.layers.conv2d(input=input,
-                                num_filters=32,
-                                filter_size=3,
-                                stride=1)
-
-    # 第一个池化层，池化大小为2*2，步长为1，最大池化
-    pool1 = fluid.layers.pool2d(input=conv1,
-                                pool_size=2,
-                                pool_stride=1,
-                                pool_type='max')
-
-    # 第二个卷积层，卷积核大小为3*3，一共有64个卷积核
-    conv2 = fluid.layers.conv2d(input=pool1,
-                                num_filters=64,
-                                filter_size=3,
-                                stride=1)
-
-    # 第二个池化层，池化大小为2*2，步长为1，最大池化
-    pool2 = fluid.layers.pool2d(input=conv2,
-                                pool_size=2,
-                                pool_stride=1,
-                                pool_type='max')
-
-    # 以softmax为激活函数的全连接输出层，大小为label大小
-    fc = fluid.layers.fc(input=pool2, size=10, act='softmax')
-    return fc
-
-
-# 获取分类器
-model = convolutional_neural_network(image)
-'''
-y_predict = fluid.layers.fc(input=image,size=1,act=None)
-
-
-# 获取损失函数和准确率函数
-cost = fluid.layers.cross_entropy(input=model, label=label)
+net=vgg_bn_drop(x)
+#定义损失函数
+cost = fluid.layers.cross_entropy(input=net,label=label)
 avg_cost = fluid.layers.mean(cost)
-acc = fluid.layers.accuracy(input=model, label=label)
+acc = fluid.layers.accuracy(input=net, label=label, k=1)
+#定义优化方法
+sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.01)
+sgd_optimizer.minimize(avg_cost)
+#数据传入设置
+batch_reader = paddle.batch(reader=dataReader(), batch_size=1024)
+testb_reader=paddle.batch(reader=testReader(), batch_size=1024)
+feeder = fluid.DataFeeder(place=cpu, feed_list=[x, label])
+prog=fluid.default_startup_program()
+exe.run(prog)
 
-# 定义优化方法
-optimizer = fluid.optimizer.AdamOptimizer(learning_rate=0.001)
-opts = optimizer.minimize(avg_cost)
+trainNum=10
+for i in range(trainNum):
+    for batch_id,data in enumerate(batch_reader()):
+        outs = exe.run(
+            feed=feeder.feed(data),
+            fetch_list=[label,avg_cost])#feed为数据表 输入数据和标签数据
+        pross=float(i)/trainNum
+        #打印输出面板
+        print(str(i+1)+"次训练后损失值为："+str(outs[1]))
+        print("当前训练进度百分比为："+str(pross*100)[:3].strip(".")+"%")
+    for batch_id,data in enumerate(testb_reader()):
+        test_acc,test_cost = exe.run(
+            feed=feeder.feed(data),
+            fetch_list=[label, avg_cost])  # feed为数据表 输入数据和标签数据
+        test_costs=[]
+        test_costs.append(test_cost[0])
+        testcost = (sum(test_costs) / len(test_costs))
+        print("预测损失为：",testcost,"\n")
 
-# 获取测试程序
-test_program = fluid.default_main_program().clone(for_test=True)
-# 进行参数初始化
-exe.run(fluid.default_startup_program())
-# 定义输入数据维度
-feeder = {'image': imgdata, 'label': textlst}
-for i in range(5):
-    results = exe.run(program=fluid.default_main_program(),
-                      feed=feeder,
-                      fetch_list=[avg_cost])
-    print(results[0])
+#保存预测模型
+path=params_dirname
+def del_file(path):
+    for i in os.listdir(path):
+        path_file = os.path.join(path, i)
+        if os.path.isfile(path_file):
+            os.remove(path_file)
+        else:
+            del_file(path_file)
+fluid.io.save_inference_model(params_dirname, ['x'],[net], exe)
