59dfd350a7a7ff483f94e6eaa85aeae8ed77130e
GT_Console <1029550448@qq.com>
2019-10-06 16:37:24 +0800

    B Yolo

--- Class_PaddlePaddle/test_07_simple_od/train.py
+++ Class_PaddlePaddle/test_07_simple_od/train.py
@@ -7,7 +7,7 @@ from GSODNet import BGSODNet
 # Hyper parameter
 use_cuda = False  # Whether to use GPU or not
 batch_size = 1  # Number of incoming batches of data
-epochs = 10 # Number of training rounds
+epochs = 10  # Number of training rounds
 save_model_path = "./model"
 data_path = "./data"
 img_size = [512, 512]
@@ -20,18 +20,18 @@ def info_read(info_path):
     """
     读取info文件下数据
     :param info_path: info文件路径
-    :return: np Array :box, label, label
+    :return:
     """
-    # 创建一个 H x W x len(box + label) 形状的数组来存储数据
-    info_array = np.zeros([block_num, 5])
+    label_list = []
+    box_list = []
     with open(info_path, "r") as f:
         infos = f.read().replace(" ", "").split("\n")
         for info_id, info in enumerate(infos):
             info = info.split(",")
             mini_data = [float(i) for i in info]
-            mini_data[-1] = int(mini_data[-1])
-            info_array[info_id] = mini_data
-        return info_array
+            label_list.append(int(mini_data[-1]))
+            box_list.append(mini_data[:4])
+        return box_list, label_list
 
 
 # Reader
@@ -46,14 +46,11 @@ def data_reader(for_test=False):
     def reader():
         for i in range(start_num, end_num):
             im = Image.open(data_path + '/img/' + str(i) + ".jpg")
-            im = np.array(im).reshape(1, 3, 512, 512).astype(np.float32)
+            im = np.array(im).transpose((2, 0, 1)).reshape(1, 3, 512, 512).astype(np.float32)
             info_path = data_path + '/info/' + str(i) + ".info"
-            info_array = info_read(info_path)
-
-            box_array = info_array[:, :4]
-            label_array = info_array[:, 4:5]
-            box_array = box_array.reshape(1, block_num, 4)
-            label_array = label_array.reshape(1, block_num, 1)
+            box_list, label_list = info_read(info_path)
+            box_array = np.array(box_list).reshape(1, -1, 4)
+            label_array = np.array(label_list).reshape(1, -1)
             img_size_array = np.array(img_size).reshape(1, 2).astype(np.int32)
             yield im, box_array, label_array, img_size_array
 
@@ -76,8 +73,8 @@ with fluid.program_guard(main_program=main_program, startup_program=startup):
     """Tips:Symbol * stands for Must"""
     # * Define data types
     img = fluid.layers.data(name="img", shape=[3, 512, 512], dtype="float32")
-    box = fluid.layers.data(name="box", shape=[block_num, 4], dtype="float32")
-    label = fluid.layers.data(name="label", shape=[block_num], dtype="int32")
+    box = fluid.layers.data(name="box", shape=[1, 4], dtype="float32", lod_level=1)
+    label = fluid.layers.data(name="label", shape=[1], dtype="int32", lod_level=1)
     img_size_2d = fluid.layers.data(name='img_size', shape=[2], dtype='int32')
     # * Access to the Network
     loss = BGSODNet(10).net(img, box, label, img_size_2d)
